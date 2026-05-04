f6f9e2b089ecb64a3b346052fcb153ba0aa5bf0c
diego <gaibo.jr.04@gmail.com>
2026-05-04 21:41:30 +0200

    modifica bat avvio diego

--- TimetablePlanner.bat
+++ TimetablePlanner.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Avvio del Timetable Planner in corso...
 
-call C:\Users\gaibo\miniconda3\Scripts\activate.bat patty
+call C:\Users\gaibo\miniconda3\Scripts\activate.bat planning
 
 cd "C:\Users\gaibo\Desktop\Universita\Sistemi Intelligenti\PDDL_timetableProject\application\GUI"
 
